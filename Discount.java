b835c3a8bbb85cffbe944afa553a46f97e29a730
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-11-30 19:58:11 +0530

    modified:   Discount.java

--- Discount.java
+++ Discount.java
@@ -1,4 +1,13 @@
 public class Discount{
+    private void input(){
+
+    }
+    private void compute1(){
+
+    }
+    private void compute2(){
+        
+    }
     public static void main(String[] args) {
         System.out.println("Hello");
     }
