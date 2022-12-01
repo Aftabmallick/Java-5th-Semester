4fe8c2030f425e632a2010f4dfc656fe20e6c6ee
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-01 19:26:13 +0530

    new file:   Account.class modified:   Account.java new file:   Current_Ac.class new file:   Savings_Ac.class new file:   TD_Ac.class

--- Account.java
+++ Account.java
@@ -1,28 +1,40 @@
 public class Account {
-    int Acc_No,  Contact_No;
+    double Acc_No,  Contact_No;
     String C_Name;
     void Show(){
         System.out.println("\nCustomer Name : "+C_Name+"\nAccount No : "+Acc_No+"\nContact No : "+Contact_No);
     }
     public static void main(String[] args) {
-        Account a1= new Savings_Ac();
-        a1.Show();
+        System.out.println("\t\t\tSavings Account\n");
+        Account sac = new Savings_Ac();
+        sac.Show();
+        System.out.println("\t\t\tCurrent Account\n");
+        Account cac = new Current_Ac();
+        cac.Show();
+        System.out.println("\t\t\tTD Account\n");
+        Account tdc = new TD_Ac();
+        tdc.Show();
 
     }
 }
 class Savings_Ac extends Account{
     private float ROI, Acc_Balance;
-  
+    Savings_Ac(int Acc_No, int Contact_No,String C_Name, float ROI, float Acc_Balance){
+        
+    }
     void Show(){
-        System.out.println("\nCustomer Name : "+super.C_Name+"\nAccount No : "+super.Acc_No+"\nContact No : "+super.Contact_No+"\nAccount Balance : "+Acc_Balance+"\nReturn of Investement : "+ROI);
+        System.out.println("\nCustomer Name : "+super.C_Name+"\nAccount No : "+super.Acc_No+"\nContact No : "+super.Contact_No+"\nAccount Balance : "+Acc_Balance+"\nReturn of Investement : "+ROI+"\n\n");
     }
 }
 class Current_Ac extends Account{
     private float Acc_Balance, Min_Balance;
     void Show(){
-        System.out.println("\nCustomer Name : "+super.C_Name+"\nAccount No : "+super.Acc_No+"\nContact No : "+super.Contact_No+"\nAccount Balance : "+Acc_Balance+"\nMinimum Balance : "+Min_Balance);
+        System.out.println("\nCustomer Name : "+super.C_Name+"\nAccount No : "+super.Acc_No+"\nContact No : "+super.Contact_No+"\nAccount Balance : "+Acc_Balance+"\nMinimum Balance : "+Min_Balance+"\n\n");
     }
 }
 class TD_Ac extends Account{
     private float Principal, Term, ROI, Maturity_Balance;
+    void Show(){
+        System.out.println("\nCustomer Name : "+super.C_Name+"\nAccount No : "+super.Acc_No+"\nContact No : "+super.Contact_No+"\nPrinciple : "+Principal+"\nReturn of Investement : "+ROI+"\nMaturity Balance : "+Maturity_Balance+"\nTerm : "+Term+"\n\n");
+    }
 }
