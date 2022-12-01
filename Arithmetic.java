d1896adcab896904c3194b585e331b7d42f56dd3
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-01 10:08:38 +0530

    new file:   Arithmetic.class new file:   Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,29 @@
+public class Arithmetic {
+    private int a;
+    private float b;
+    private char c;
+    private double d, sum;
+    Arithmetic(int a, float b, char c, double d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    void add(int a, double d) {
+        a = this.a;
+        d = this.d;
+        this.sum = a + d;
+    }
+
+    void display() {
+        System.out.println("Sum = " + this.sum);
+    }
+
+    public static void main(String[] args) {
+        Arithmetic a1 = new Arithmetic(10, 10.5f, 'a', 500);
+        a1.add(10, 10);
+        a1.display();
+
+    }
+}
