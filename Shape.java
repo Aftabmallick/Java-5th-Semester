1ec3b64ebb43a7b9e9c205e62d5fd055e62357ca
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-02 20:42:38 +0530

    modified:   Shape.java

--- Shape.java
+++ Shape.java
@@ -1,4 +1,7 @@
+import java.util.*;
 public class Shape {
+    protected int length,height;
+    Scanner sc = new Scanner(System.in)
     void get(){
 
     }
@@ -6,9 +9,22 @@ public class Shape {
     
     }
     void cal_area(){
-        
+
     }
    public static void main(String[] args) {
     
    } 
+}
+class Square extends Shape{
+    int a;
+    void get(){
+        
+    }
+    void put(){
+    
+    }
+    void cal_area(){
+
+    }
+
 }
