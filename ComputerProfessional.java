380eb55a0e2d4507c1055ae444a22aefa15c7cda
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-02 11:49:11 +0530

    new file: ComputerProfessional.java

---
+++ ComputerProfessional.java
@@ -0,0 +1,16 @@
+public class ComputerProfessional {
+    private String EName,Prospect;
+    private int DutyHour;
+    public static void main(String[] args) {
+        
+    }
+}
+class Developer extends ComputerProfessional{
+    
+}
+class Developer extends NetworkAdmin{
+
+}
+class Developer extends DataOperator{
+    
+}
