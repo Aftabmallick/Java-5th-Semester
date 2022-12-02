bac7dd96d0c2d1bcd1af44f7b19eca50d8af3c7f
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-02 21:29:50 +0530

    modified:   Shape.java

--- Shape.java
+++ Shape.java
@@ -16,6 +16,10 @@ public class Shape {
         sq.get();
         sq.cal_area();
         sq.put();
+        Shape r=new Rectangle();
+        r.get();
+        r.cal_area();
+        r.put();
    } 
 }
 class Square extends Shape{
@@ -30,6 +34,38 @@ class Square extends Shape{
     }
     void cal_area(){
         a=length*length;
+    }
+
+}
+class Rectangle extends Shape{
+    int a;
+    void get(){
+        System.out.println("Enter The length & Height: ");
+        length = sc.nextInt();
+        height = sc.nextInt();
+
+    }
+    void put(){
+        System.out.println("Area = "+a);
+    }
+    void cal_area(){
+        a=length*height;
+    }
+    
+}
+class triangle extends Shape{
+    int a;
+    void get(){
+        System.out.println("Enter The length & Height: ");
+        length = sc.nextInt();
+        height = sc.nextInt();
+
+    }
+    void put(){
+        System.out.println("Area = "+a);
+    }
+    void cal_area(){
+        a=length*height;
     }
 
 }
