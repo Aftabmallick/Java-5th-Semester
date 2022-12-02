71bce678396e0efa2e4c83ef73dfe5d13ed9768b
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-02 19:38:40 +0530

    modified:   ComputerProfessional.java

--- ComputerProfessional.java
+++ ComputerProfessional.java
@@ -1,22 +1,27 @@
 public class ComputerProfessional {
     private String EName,Prospect;
     private int DutyHour;
+    ComputerProfessional(String EName,String Prospect,int DutyHour){
+        this.DutyHour=DutyHour;
+        this.EName=EName;
+        this.Prospect=Prospect;
+    }
     public static void main(String[] args) {
         
     }
 }
 class Developer extends ComputerProfessional{
-    
+    private int StudyHour;
 }
-class Developer extends NetworkAdmin{
-
+class NetworkAdmin extends ComputerProfessional{
+    private int PracticeHours;
 }
-class Developer extends DataOperator{
-
+class DataOperator extends ComputerProfessional{
+    private int TypingSpeed;
 }
-class Developer extends JavaProfessional{
-
+class JavaProfessional extends Developer{
+    private int ProficiencyLevel;
 }
-class Developer extends PythonProfessional{
-    
+class PythonProfessional extends Developer{
+    private int ProficiencyLevel;
 }
