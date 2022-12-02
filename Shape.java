a03aa1b8f1c1784a72339a6b7e2e30b287a1c807
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-02 20:47:11 +0530

    modified:   Shape.java

--- Shape.java
+++ Shape.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Shape {
     protected int length,height;
-    Scanner sc = new Scanner(System.in)
+    Scanner sc = new Scanner(System.in);
     void get(){
 
     }
@@ -12,19 +12,24 @@ public class Shape {
 
     }
    public static void main(String[] args) {
-    
+        Shape sq=new Square();
+        sq.get();
+        sq.cal_area();
+        sq.put();
    } 
 }
 class Square extends Shape{
     int a;
     void get(){
-        
+        System.out.println("Enter The length : ");
+        length=sc.nextInt();
+
     }
     void put(){
-    
+        System.out.println("Area = "+a);
     }
     void cal_area(){
-
+        a=length*length;
     }
 
 }
