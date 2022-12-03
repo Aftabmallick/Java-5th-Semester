f3fd02be5909b195cf794f6323c333bb3cdb29a1
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-03 12:54:29 +0530

    modified:   Assignment7.java

--- Assignment7.java
+++ Assignment7.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public class Assignment7 {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        int a;
-        char c;
-        float f;
-        double d;
+        int a,aa;
+        char c,cc;
+        float f,ff;
+        double d,dd;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter integer value : ");
         a =sc.nextInt();
@@ -16,6 +19,26 @@ public class Assignment7 {
         f=sc.nextFloat();
         System.out.println("nter any double value : ");
         d=sc.nextDouble();
+
+        System.out.println("\n Wrapping : ");
+		int A= Integer.valueOf(a);
+		char C= Character.valueOf(c);
+		float F= Float.valueOf(f);
+		double D= Double.valueOf(d);
+        System.out.println(a);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(d);
+
+        System.out.println("\n Unwrapping : ");
+		aa=A.intValue();
+		cc=C.charValue();
+		ff=F.floatValue();
+		dd=D.doubleValue();
+        System.out.println(a);
+		System.out.println(c);
+		System.out.println(f);
+		System.out.println(d);
 
     }
 }
