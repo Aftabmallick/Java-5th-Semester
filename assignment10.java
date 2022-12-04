ff5134f1427a0ae125708bc259187ae208bf2f09
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-04 22:10:56 +0530

    new file:   area.class new file:   assignment10.class modified:   assignment10.java new file:   circle.class new file:   rectangle.class

--- assignment10.java
+++ assignment10.java
@@ -1,9 +1,35 @@
 interface area{
-    final public static float pi=3.14;
+    final public static float pi=3.14f;
     public abstract void cal_area();
 }
+class circle implements area{
+    private float r,area;
+    circle(float r){
+        this.r=r;
+    }
+    public void cal_area(){
+        area=pi*r*r;
+        System.out.println(" The Area of Circle is : "+area);
+    }
+}
+class rectangle implements area{
+    private float l,b,area;
+    rectangle(float l,float b){
+        this.l=l;
+        this.b=b;
+    }
+    public void cal_area(){
+        area=l*b;
+        System.out.println("The area of a rectangle is : "+area);
+    }
+
+}
+
 public class assignment10 {
     public static void main(String[] args) {
-        
+        circle c1 =new circle(10);
+        c1.cal_area();
+        rectangle r1 =new rectangle(10, 20);
+        r1.cal_area();
     }
 }
