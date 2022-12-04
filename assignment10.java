f0873a61b2bfec0f983ae0c93cad8466359aff62
Aftab Mallick <78075420+Aftabmallick@users.noreply.github.com>
2022-12-04 21:59:40 +0530

    modified:   assignment10.java

--- assignment10.java
+++ assignment10.java
@@ -1,3 +1,9 @@
+interface area{
+    final public static float pi=3.14;
+    public abstract void cal_area();
+}
 public class assignment10 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
